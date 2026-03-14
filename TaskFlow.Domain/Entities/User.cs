3b2a6c86495ba32cf3053f4e6819b050f8a93cc8
VictorN <victor.ndou@redpandasoftware.co.za>
2026-03-14 11:27:13 +0200

    Add project files.

---
+++ TaskFlow.Domain/Entities/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.ValueObjects;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Email Email { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        private User() { }
+        public User(string name, Email email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be empty.");
+
+            Name = name;
+        }
+
+        public void UpdateEmail( Email email)
+        {
+            if (email == null)
+                throw new ArgumentException("Email cannot be empty.");
+            Email = email;
+        }
+    }
+}
---
+++ TaskFlow.Domain/ValueObjects/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.ValueObjects
+{
+    public class Email
+    {
+        public string Value { get; }
+
+        public Email(string value)
+        {
+            if ((string.IsNullOrWhiteSpace(value)))
+                throw new ArgumentException("Email cannot be empty.");
+
+            if(!value.Contains("@") || !value.Contains("."))
+                throw new ArgumentException("Email is not valid.");
+
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
